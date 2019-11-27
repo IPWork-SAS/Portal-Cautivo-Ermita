1ab80423b416c3c848e0df103f7ca554fb50d1b4
fearovex <32711883+fearovex@users.noreply.github.com>
2019-11-27 15:50:37 -0500

    First Commit

---
+++ lang/es.php
@@ -0,0 +1,25 @@
+<?php
+    $lang = array(
+        "lang" => "es",
+        "titulo_website" => "Portal Cautivo Hotel Ermita",
+        "titulo_form" => "Bienvenido a nuestra red de WIFI.",
+        "nombre_form" => "Nombre",
+        "apellidos_form" => "Apellidos",
+        "email_form" => "Correo Electrónico",
+        "indicativo_form" => "Cod.",
+        "celular_form" => "Número Celular",
+        "validacion_celular_form" => "Ingrese un número de telefono celular valido.",
+        "edad_form" => "Edad",
+        "seleccion_genero_form" => "Seleccione el género",
+        "masculino_genero_form" => "Masculino",
+        "femenino_genero_form" => "Femenino",
+        "otro_genero_form" => "Otro",
+        "terminos_link" => "Aceptar Terminos y Condiciones",
+        "btn_continuar" => "Continuar",
+        "titulo_banner" => "Conéctate y disfruta de nuestra red WIFI.",
+        "btn_conectar" => "Conectar",
+        "error_url" => "Ops! Algo salio mal. Pongase en contacto con el administrador de la red.",
+        "error_licencia" => "Ops! La licencia ha caducado. Pongase en contacto con el administrador de la red.",
+        "bienvenido_usuario" => "Bienvenido "
+    );
+?>
